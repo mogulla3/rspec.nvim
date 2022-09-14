ccde9ebc4cdc6c58f641a5eb3b51aa6cc1f2d34f
mogulla3 <hs.iarn3@gmail.com>
2022-09-14 09:46:51 +0900

    Refactor

--- lua/rspec/viewer.lua
+++ lua/rspec/viewer.lua
@@ -16,22 +16,20 @@ function Viewer.open_last_spec_result_window()
   end
 
   -- margin: 5 10
-  local win_width = vim.fn.winwidth(0)
-  local win_height = vim.fn.winheight(0)
-
-  local opts = {
+  local win_opts = {
     relative = "editor",
     row = 5,
     col = 10,
-    width = win_width - 20,
-    height = win_height - 10,
+    width = vim.fn.winwidth(0) - 20,
+    height = vim.fn.winheight(0) - 10,
     style = "minimal",
     border = "rounded",
   }
 
-  last_spec_result_win_id = vim.api.nvim_open_win(bufnr, config.focus_on_last_spec_result_window, opts)
-  vim.api.nvim_win_set_buf(last_spec_result_win_id, bufnr)
+  last_spec_result_win_id = vim.api.nvim_open_win(bufnr, config.focus_on_last_spec_result_window, win_opts)
   vim.api.nvim_win_set_option(last_spec_result_win_id, "wrap", true)
+  vim.api.nvim_win_set_option(last_spec_result_win_id, "winhl", "Normal:ErrorFloat")
+  vim.api.nvim_win_set_buf(last_spec_result_win_id, bufnr)
 
   local buf_content = nil
   if vim.g.last_command_stdout then
@@ -42,8 +40,6 @@ function Viewer.open_last_spec_result_window()
 
   buf_content = vim.list_extend(buf_content, { "", "* Close the window with the following keys : q, <Esc>, <CR>" })
   vim.api.nvim_buf_set_lines(bufnr, 0, 0, true, buf_content)
-
-  vim.api.nvim_win_set_option(last_spec_result_win_id, "winhl", "Normal:ErrorFloat")
 end
 
 return Viewer
